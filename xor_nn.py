43fb317c727e0a1616ee51b2b1deca6180d04d9f
Zac Monroe <zmonroe0820@gmail.com>
2018-09-15 11:16:26 -0400

    Added activation function for xor

--- xor_nn.py
+++ xor_nn.py
@@ -5,3 +5,14 @@ from pylab import rcParams
 import math
 
 # Goal : build a NN that can model the XOR gate
+
+# Using this website as a guide
+# https://www.analyticsindiamag.com/beginners-guide-neural-network-math-python/
+
+# Activation function
+def sigmoid(x):
+  return 1 / (1 + math.e ** (-1 * x))
+
+# Derivative of activation function
+def sigmoid_derivative(x):
+  return (sigmoid(x) * (1 - sigmoid(x)))
