fa1da88f98289beeb73fc4f043bd03a769bce33f
Zac Monroe <zmonroe0820@gmail.com>
2018-09-15 11:21:06 -0400

    Set up input/output

--- xor_nn.py
+++ xor_nn.py
@@ -16,3 +16,9 @@ def sigmoid(x):
 # Derivative of activation function
 def sigmoid_derivative(x):
   return (sigmoid(x) * (1 - sigmoid(x)))
+
+# Inputs to the XOR gate
+X = np.array([[0,0], [0,1], [1,0], [1,1]])
+
+# Desired outputs of XOR gate for each input in X
+y = np.array([[0],   [1],   [1],   [0]])
